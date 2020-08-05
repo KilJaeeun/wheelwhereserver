bee50a070597af8bd3a3370eb9354e35b47924dc
KilJaeeun <rha3122@naver.com>
2020-08-05 21:15:47 +0900

    change database to wheel

--- wheelwhere_server/wheelwhere_server/settings.py
+++ wheelwhere_server/wheelwhere_server/settings.py
@@ -78,10 +78,19 @@ WSGI_APPLICATION = 'wheelwhere_server.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'wheel',
+        'USER': 'kil',
+        'PASSWORD': '12',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'POOL_OPTIONS': {
+            'POOL_SIZE': 10,
+            'MAX_OVERFLOW': 10
+        }
     }
 }
+
 
 
 # Password validation
